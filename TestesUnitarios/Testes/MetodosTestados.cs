2607bf4bdbf0e470076fa636d37a2b585a7512e5
Thiago Paiva  Medeiros <thiagopaivamed@gmail.com>
2019-03-21 19:03:46 -0300

    Testes unitários

---
+++ TestesUnitarios/Testes/MetodosTestados.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Testes
+{
+    public class MetodosTestados
+    {
+        [Fact]
+        public void Somar()
+        {
+            Assert.NotEqual(5, 4 + 4);
+        }
+
+        [Theory]
+        [InlineData("Paralelepipedo", "Para")]
+        [InlineData("Aviao", "vi")]
+        [InlineData("Atualmente", "mente")]
+        public void VerificaSubString(string palavra, string subpalavra)
+        {
+            Assert.Contains(subpalavra, palavra);
+        }
+    }
+}
